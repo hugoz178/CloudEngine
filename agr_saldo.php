959eda3772367c9ba1995ead25a692accf27070d
Diero_999 <57187529+Dieromen999@users.noreply.github.com>
2022-03-23 12:20:37 -0600

    ya no se pueden numeros negativos

--- agr_saldo.php
+++ agr_saldo.php
@@ -5,14 +5,19 @@ require_once 'php/conexion.php';
 $camp = $_SESSION['username'];
 $saldo=$_POST['saldo'];
 
+if($saldo>0){
 
-$insertar=("INSERT into saldo (saldo,usuario) values('$saldo','$camp')");
-$resultado=mysqli_query($cnx,$insertar);
+    $insertar=("INSERT into saldo (saldo,usuario) values('$saldo','$camp')");
+    $resultado=mysqli_query($cnx,$insertar);
 
-if ($resultado) {
-  header("Location:saldo.php?correcto=1");
-}else
-{
+    if ($resultado) {
+        header("Location:saldo.php?correcto=1");
+    }else{
 	header("Location:ag_saldo.php?error=1");
-}
+    }
+
+    }else{
+        header("Location:ag_saldo.php?error=1");
+    }
+
 ?>
