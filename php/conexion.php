400afca50d248f6dc77fb2f7e5342ec0eed520cc
hugoz178 <18040046@alumno.utc.edu.mx>
2022-03-22 19:51:34 -0600

    Actulización
    Elimine archivios

--- php/conexion.php
+++ php/conexion.php
@@ -6,6 +6,6 @@ $cnx = mysqli_connect("localhost","root","","cloudengine") or die("Error De Cone
 
 #CONEXION REMOTA
 
-#$cnx =mysqli_connect("localhost","id16322163_hugoz178","Zarcortgame1.","id16322163_michis") or die("La BD No Existe");
+#$cnx = mysqli_connect("fdb34.awardspace.net","4070406_cloudengine","Zarcortgame1.","4070406_cloudengine");
 
 ?>
